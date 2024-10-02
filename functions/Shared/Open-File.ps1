a1dd4d9fb07ab560b76feede7c2385563957ba0e
Connor <cdubyamason@gmail.com>
2024-10-02 03:51:52 -0400

    formatting

--- functions/Shared/Open-File.ps1
+++ functions/Shared/Open-File.ps1
@@ -1,43 +1,43 @@
 function Open-File {
-<#
-    .SYNOPSIS
-        Allows the user to Open a file for processing.
+    <#
+        .SYNOPSIS
+            Allows the user to Open a file for processing.
 
-    .DESCRIPTION
-        Opens a file dialog that lets the user Open a file. The file type can be specified; defaults to all file types.
+        .DESCRIPTION
+            Opens a file dialog that lets the user Open a file. The file type can be specified; defaults to all file types.
 
-    .PARAMETER FileType
-        The file extension to filter by in the file dialog. Default is "All files (*.*)".
+        .PARAMETER FileType
+            The file extension to filter by in the file dialog. Default is "All files (*.*)".
 
-    .PARAMETER Title
-        The title of the file dialog. Default is "Open a File".
+        .PARAMETER Title
+            The title of the file dialog. Default is "Open a File".
 
-    .PARAMETER InitialDirectory
-        The initial directory to display in the file dialog. Default is the directory of the script.
+        .PARAMETER InitialDirectory
+            The initial directory to display in the file dialog. Default is the directory of the script.
 
-    .EXAMPLE
-        Open-File -FileType "*.txt"
-        Opens a dialog allowing only text files to be selected.
+        .EXAMPLE
+            Open-File -FileType "*.txt"
+            Opens a dialog allowing only text files to be selected.
 
-    .EXAMPLE
-        Open-File
-        Opens a dialog allowing any file type to be selected.
+        .EXAMPLE
+            Open-File
+            Opens a dialog allowing any file type to be selected.
 
-    .OUTPUTS 
-        String
-        The full path of the selected file. If canceled, exits the script.
-    
-    .NOTES
-        Author: Connor Mason
-        Date: 10/2/2024
-#>
-[CmdletBinding()]
-Param(
-    [ValidatePattern('^\*\.[a-zA-Z0-9]+$|^All files \(\*\.\*\)$')]
-    [string]$FileType = "All files (*.*)",
-    [string]$Title = "Select a File",
-    [string]$InitialDirectory = ""
-)
+        .OUTPUTS 
+            String
+            The full path of the selected file. If canceled, exits the script.
+        
+        .NOTES
+            Author: Connor Mason
+            Date: 10/2/2024
+    #>
+    [CmdletBinding()]
+    Param(
+        [ValidatePattern('^\*\.[a-zA-Z0-9]+$|^All files \(\*\.\*\)$')]
+        [string]$FileType = "All files (*.*)",
+        [string]$Title = "Select a File",
+        [string]$InitialDirectory = ""
+    )
     $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
     $openFileDialog.Title = $Title
     $openFileDialog.Filter = "$FileType|$FileType"
--- functions/Shared/Process-CSV-File.ps1
+++ functions/Shared/Process-CSV-File.ps1
@@ -1,53 +1,53 @@
 . "../Shared/Compare-Strings.ps1"
 function Process-CSV-File {
-<#
-    .SYNOPSIS
-        Finds matching strings within a specified tolerance in a CSV file. Either returns the entire rows or just the matching values.
+    <#
+        .SYNOPSIS
+            Finds matching strings within a specified tolerance in a CSV file. Either returns the entire rows or just the matching values.
 
-    .DESCRIPTION
-        The Process-CSV-File function searches for matching strings within a specified tolerance in a CSV file. 
-        It compares the search value with the values in the specified column of the CSV file and returns either the matching strings or the entire row.
+        .DESCRIPTION
+            The Process-CSV-File function searches for matching strings within a specified tolerance in a CSV file. 
+            It compares the search value with the values in the specified column of the CSV file and returns either the matching strings or the entire row.
 
-    .PARAMETER SelectedFile
-        Specifies the path to the CSV file. Must be CSV File because it searches by column name and optionally returns the entire row.
+        .PARAMETER SelectedFile
+            Specifies the path to the CSV file. Must be CSV File because it searches by column name and optionally returns the entire row.
 
-    .PARAMETER searchVal
-        Specifies the value to search for within the CSV file.
+        .PARAMETER searchVal
+            Specifies the value to search for within the CSV file.
 
-    .PARAMETER columnName
-        Specifies the name of the column in the CSV file to search within.
+        .PARAMETER columnName
+            Specifies the name of the column in the CSV file to search within.
 
-    .PARAMETER tolerance
-        Specifies the tolerance level for string comparison. Only strings within this tolerance level will be considered as matches.
+        .PARAMETER tolerance
+            Specifies the tolerance level for string comparison. Only strings within this tolerance level will be considered as matches.
 
-    .PARAMETER returnWholeRow
-        Specifies whether to return the entire row or just the matching value. Use "y" to return the entire row, and any other value to return just the matching value.
+        .PARAMETER returnWholeRow
+            Specifies whether to return the entire row or just the matching value. Use "y" to return the entire row, and any other value to return just the matching value.
 
-    .EXAMPLE
-        Process-CSV-File -SelectedFile "C:\path\to\file.csv" -searchVal "apple" -columnName "Fruit" -tolerance 2 -returnWholeRow "y"
-        This example searches for the value "apple" within the "Fruit" column of the CSV file located at "C:\path\to\file.csv". It allows a tolerance of 2 characters for string comparison and returns the entire row for each matching string.
+        .EXAMPLE
+            Process-CSV-File -SelectedFile "C:\path\to\file.csv" -searchVal "apple" -columnName "Fruit" -tolerance 2 -returnWholeRow "y"
+            This example searches for the value "apple" within the "Fruit" column of the CSV file located at "C:\path\to\file.csv". It allows a tolerance of 2 characters for string comparison and returns the entire row for each matching string.
 
-    .EXAMPLE
-        Process-CSV-File -SelectedFile "C:\path\to\file.csv" -searchVal "banana" -columnName "Fruit" -tolerance 1 -returnWholeRow "n"
-        This example searches for the value "banana" within the "Fruit" column of the CSV file located at "C:\path\to\file.csv". It allows a tolerance of 1 character for string comparison and returns only the matching value for each match.
+        .EXAMPLE
+            Process-CSV-File -SelectedFile "C:\path\to\file.csv" -searchVal "banana" -columnName "Fruit" -tolerance 1 -returnWholeRow "n"
+            This example searches for the value "banana" within the "Fruit" column of the CSV file located at "C:\path\to\file.csv". It allows a tolerance of 1 character for string comparison and returns only the matching value for each match.
 
-    .OUTPUTS
-        System.Object[] | string[] 
-        An array of matching strings or rows from the CSV file, depending on the returnWholeRow parameter.
+        .OUTPUTS
+            System.Object[] | string[] 
+            An array of matching strings or rows from the CSV file, depending on the returnWholeRow parameter.
 
-    .NOTES
-        Author: Connor Mason
-        Date:   10/2/2024
+        .NOTES
+            Author: Connor Mason
+            Date:   10/2/2024
 
-#>
-[CmdletBinding()]
-param (
-    [string]$SelectedFile,
-    [string]$searchVal,
-    [string]$columnName,
-    [int]$tolerance,
-    [string]$returnWholeRow
-)
+    #>
+    [CmdletBinding()]
+    param (
+        [string]$SelectedFile,
+        [string]$searchVal,
+        [string]$columnName,
+        [int]$tolerance,
+        [string]$returnWholeRow
+    )
     $csvData = Import-Csv -Path $SelectedFile
     $matchingStringsResults = @()
     foreach ($row in $csvData) {
